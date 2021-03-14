83e551b620fae39a5ec53825d9e191f15cf5d42e
Dugnist <alex.dugnist@gmail.com>
2021-03-14 19:06:24 +0200

    expo branch

--- App.js
+++ App.js
@@ -10,5 +10,5 @@
 import App from "./app/app.tsx"
 import { AppRegistry } from "react-native"
 
-AppRegistry.registerComponent("mobileapp", () => App)
+AppRegistry.registerComponent("PizzaApp", () => App)
 export default App
